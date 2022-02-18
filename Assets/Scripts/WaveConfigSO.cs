5eaeed251e70ba5fe940fceeb4217dcc9350d71a
KewinAl <allenspachkewin@gmail.com>
2022-02-18 15:00:15 +0100

    Instantiating Enemies

--- Assets/Scripts/WaveConfigSO.cs
+++ Assets/Scripts/WaveConfigSO.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "WaveConfig", fileName = "New Wave Config")]
 public class WaveConfigSO : ScriptableObject
 {
+    [SerializeField] List<GameObject> enemyPrefabs;
     [SerializeField] Transform pathPrefab;
     [SerializeField] float moveSpeed = 5f;
 
+    public float GetEnemyCount()
+    {
+        return enemyPrefabs.Count;
+    }
+    public GameObject GetEnemyPrefab(int index)
+    {
+        return enemyPrefabs[index];
+    }
     public Transform getStartWaypoint()
     {
         return pathPrefab.GetChild(0);
